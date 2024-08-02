9f6f4a15a7c71823a521cabf42fb1a1b58cea2ed
노현주 <dev.hyeonju@gmail.com>
2024-08-02 10:33:19 +0900

    start :: 프로젝트 시작

---
+++ src/main/kotlin/com/ondosee/OndoseeApplication.kt
@@ -0,0 +1,11 @@
+package com.ondosee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OndoseeApplication
+
+fun main(args: Array<String>) {
+	runApplication<OndoseeApplication>(*args)
+}
---
+++ src/test/kotlin/com/ondosee/OndoseeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ondosee
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OndoseeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
