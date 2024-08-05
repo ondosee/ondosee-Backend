779d42ce6124672919161af41a5377d42f4a48be
노현주 <dev.hyeonju@gmail.com>
2024-08-05 19:36:07 +0900

    add :: open feign 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,18 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:Hoxton.SR9")
+	}
 }
 
 kotlin {
--- src/main/kotlin/com/ondosee/OndoseeApplication.kt
+++ src/main/kotlin/com/ondosee/OndoseeApplication.kt
@@ -2,7 +2,9 @@ package com.ondosee
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients
 @SpringBootApplication
 class OndoseeApplication
 
