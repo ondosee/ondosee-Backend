7473d3d85d55211480464faed936a8dbbee94e3d
노현주 <dev.hyeonju@gmail.com>
2024-08-07 16:06:24 +0900

    create :: vworld mapper

---
+++ src/main/kotlin/com/ondosee/thirdparty/vworld/mapper/VWorldMapper.kt
@@ -0,0 +1,35 @@
+package com.ondosee.thirdparty.vworld.mapper
+
+import com.ondosee.thirdparty.vworld.data.res.PageResponseData
+import com.ondosee.thirdparty.vworld.data.res.PointResponseData
+import com.ondosee.thirdparty.vworld.data.res.ResultResponseData
+import com.ondosee.thirdparty.vworld.data.res.SearchDistrictResponseData
+import com.ondosee.thirdparty.vworld.data.web.Page
+import com.ondosee.thirdparty.vworld.data.web.Point
+import com.ondosee.thirdparty.vworld.data.web.SearchDistrictResultWebResponse
+import com.ondosee.thirdparty.vworld.data.web.SearchDistrictVWoldWebResponse
+
+fun SearchDistrictVWoldWebResponse.toResponse() = SearchDistrictResponseData(
+    status = response.status,
+    page = response.page!!.toResponse(),
+    result = response.result?.toResponse() ?: listOf()
+)
+
+fun SearchDistrictResultWebResponse.toResponse() =
+    items.map {
+        ResultResponseData(
+            title = it.title,
+            point = it.point.toResponse()
+        )
+    }
+
+fun Page.toResponse() = PageResponseData(
+    total = total,
+    current = current,
+    size = size
+)
+
+fun Point.toResponse() = PointResponseData(
+    x = x,
+    y = y
+)
