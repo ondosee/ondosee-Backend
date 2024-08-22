4cae79fddc3883634b55705ca280962f9c93833a
노현주 <dev.hyeonju@gmail.com>
2024-08-22 21:06:25 +0900

    update :: 매개변수 설정

--- src/main/kotlin/com/ondosee/common/spi/WeatherPort.kt
+++ src/main/kotlin/com/ondosee/common/spi/WeatherPort.kt
@@ -1,7 +1,8 @@
 package com.ondosee.common.spi
 
+import com.ondosee.domain.weather.service.data.req.GetTodayWeatherRequestData
 import com.ondosee.domain.weather.service.data.res.GetTodayWeatherResponseData
 
 interface WeatherPort {
-    fun getTodayWeather(request: GetTodayWeatherResponseData): GetTodayWeatherResponseData
+    fun getTodayWeather(request: GetTodayWeatherRequestData): GetTodayWeatherResponseData
 }
