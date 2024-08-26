ca7be339b529b3885b40d49c95d4acac91537f2b
노현주 <dev.hyeonju@gmail.com>
2024-08-26 18:11:33 +0900

    update :: open weather property 값 추가

--- src/main/kotlin/com/ondosee/global/config/PropertiesScanConfig.kt
+++ src/main/kotlin/com/ondosee/global/config/PropertiesScanConfig.kt
@@ -2,6 +2,7 @@ package com.ondosee.global.config
 
 import com.ondosee.global.config.properties.RedisProperties
 import com.ondosee.thirdparty.dataportal.data.properties.DataPorterProperties
+import com.ondosee.thirdparty.openweather.data.properties.OpenWeatherProperties
 import com.ondosee.thirdparty.vworld.data.properties.VWorldProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration
     basePackageClasses = [
         VWorldProperties::class,
         DataPorterProperties::class,
+        OpenWeatherProperties::class,
         RedisProperties::class
     ]
 )
