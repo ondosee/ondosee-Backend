c7dc6e8432f823ba7e2ad46429b5cde471087e78
uuuuuuuk <s22034@gsm.hs.kr>
2024-09-19 13:51:41 +0900

    update :: alarmTime 타입 변경, constructor를 통해 초기화 하도록 변경

--- src/main/kotlin/com/ondosee/domain/notification/domain/entity/Notification.kt
+++ src/main/kotlin/com/ondosee/domain/notification/domain/entity/Notification.kt
@@ -1,6 +1,5 @@
 package com.ondosee.domain.notification.domain.entity
 
-import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
@@ -9,11 +8,13 @@ data class Notification(
     @Id
     @Column(name = "notification_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
+    val id: Long,
 
     @Column(name = "device_token")
     val deviceToken: String,
 
     @Column(name = "alarm_time")
-    var alarmTime: LocalDateTime?
-)
+    val alarmTime: String?
+) {
+    constructor() : this(0, "", null)
+}
