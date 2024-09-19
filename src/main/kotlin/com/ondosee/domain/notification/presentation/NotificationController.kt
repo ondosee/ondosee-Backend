3843b4aa894d588b460e068fb4208313f1af83ea
uuuuuuuk <s22034@gsm.hs.kr>
2024-09-19 13:53:41 +0900

    update :: 디바이스 토큰 저장 API 삭제 및 알림 시간 설정 setAlarm 함수 추가

--- src/main/kotlin/com/ondosee/domain/notification/presentation/NotificationController.kt
+++ src/main/kotlin/com/ondosee/domain/notification/presentation/NotificationController.kt
@@ -1,12 +1,13 @@
 package com.ondosee.domain.notification.presentation
 
+import com.ondosee.domain.notification.presentation.web.req.SetAlarmWebRequest
 import com.ondosee.domain.notification.service.NotificationService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/notification")
@@ -14,7 +15,7 @@ class NotificationController(
     private val notificationService: NotificationService
 ) {
     @PostMapping
-    fun saveNotification(@RequestParam deviceToken: String): ResponseEntity<Void> =
-        notificationService.saveNotification(deviceToken)
+    fun setAlarm(@Valid webRequest: SetAlarmWebRequest): ResponseEntity<Void> =
+        notificationService.setAlarm(webRequest)
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
 }
