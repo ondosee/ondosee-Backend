762a4c74fc3121e8c560a8187b001b5b72ac4397
uuuuuuuk <s22034@gsm.hs.kr>
2024-09-19 13:56:53 +0900

    update :: saveNotification 함수 삭제 및 setAlarm 함수 구현

--- src/main/kotlin/com/ondosee/domain/notification/service/NotificationServiceImpl.kt
+++ src/main/kotlin/com/ondosee/domain/notification/service/NotificationServiceImpl.kt
@@ -1,13 +1,22 @@
 package com.ondosee.domain.notification.service
 
 import com.ondosee.common.spi.notification.CommandNotificationPort
+import com.ondosee.common.spi.notification.QueryNotificationPort
+import com.ondosee.domain.notification.presentation.web.req.SetAlarmWebRequest
 import org.springframework.stereotype.Service
 
 @Service
 class NotificationServiceImpl(
+    private val queryNotificationPort: QueryNotificationPort,
     private val commandNotificationPort: CommandNotificationPort
 ) : NotificationService {
-    override fun saveNotification(deviceToken: String) {
-        commandNotificationPort.saveDeviceToken(deviceToken)
+    override fun setAlarm(webRequest: SetAlarmWebRequest) {
+        with(webRequest) {
+            queryNotificationPort.findByDeviceToken(deviceToken)?.let {
+                commandNotificationPort.deleteDeviceToken(it)
+            }
+
+            commandNotificationPort.saveAlarm(deviceToken, alarmTime)
+        }
     }
 }
