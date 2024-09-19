d04ec3f0743d316849a7fe85b56052c9215a0a56
uuuuuuuk <s22034@gsm.hs.kr>
2024-09-19 15:27:17 +0900

    update :: RequestParam -> RequestBody

--- src/main/kotlin/com/ondosee/domain/notification/presentation/NotificationController.kt
+++ src/main/kotlin/com/ondosee/domain/notification/presentation/NotificationController.kt
@@ -5,6 +5,7 @@ import com.ondosee.domain.notification.service.NotificationService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
@@ -15,7 +16,7 @@ class NotificationController(
     private val notificationService: NotificationService
 ) {
     @PostMapping
-    fun setAlarm(@Valid webRequest: SetAlarmWebRequest): ResponseEntity<Unit> =
+    fun setAlarm(@Valid @RequestBody webRequest: SetAlarmWebRequest): ResponseEntity<Unit> =
         notificationService.setAlarm(webRequest)
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
 }
--- src/main/kotlin/com/ondosee/domain/notification/presentation/web/req/SetAlarmWebRequest.kt
+++ src/main/kotlin/com/ondosee/domain/notification/presentation/web/req/SetAlarmWebRequest.kt
@@ -1,13 +1,10 @@
 package com.ondosee.domain.notification.presentation.web.req
 
-import org.springframework.web.bind.annotation.RequestParam
 import javax.validation.constraints.Pattern
 
 data class SetAlarmWebRequest(
-    @RequestParam
     val deviceToken: String,
 
-    @RequestParam
     @field:Pattern(regexp = "^([01]\\d|2[0-3]):([0-5]\\d)$")
     val alarmTime: String
 )
