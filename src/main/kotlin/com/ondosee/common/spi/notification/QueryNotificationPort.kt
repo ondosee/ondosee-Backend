3f49e9f95c9a9518bc77ac52ecf7bf80a31ba45b
uuuuuuuk <s22034@gsm.hs.kr>
2024-09-20 15:09:14 +0900

    update :: findByAlarmTime 함수 구현

--- src/main/kotlin/com/ondosee/common/spi/notification/QueryNotificationPort.kt
+++ src/main/kotlin/com/ondosee/common/spi/notification/QueryNotificationPort.kt
@@ -1,5 +1,8 @@
 package com.ondosee.common.spi.notification
 
+import com.ondosee.domain.notification.domain.entity.Notification
+
 interface QueryNotificationPort {
     fun existByDeviceToken(deviceToken: String): Boolean
+    fun findByAlarmTime(alarmTime: String): List<Notification>
 }
