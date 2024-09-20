289caa35dd080f9baac0978bbf8a3a522c873abd
uuuuuuuk <s22034@gsm.hs.kr>
2024-09-20 14:40:03 +0900

    update :: sendAlarm 함수 구현

--- src/main/kotlin/com/ondosee/domain/notification/service/NotificationServiceImpl.kt
+++ src/main/kotlin/com/ondosee/domain/notification/service/NotificationServiceImpl.kt
@@ -1,14 +1,20 @@
 package com.ondosee.domain.notification.service
 
 import com.ondosee.common.spi.notification.CommandNotificationPort
+import com.ondosee.common.spi.notification.NotificationPort
 import com.ondosee.common.spi.notification.QueryNotificationPort
+import com.ondosee.domain.notification.domain.entity.NotificationAlarm
 import com.ondosee.domain.notification.presentation.web.req.SetAlarmWebRequest
+import com.ondosee.thirdparty.firebase.data.enums.NotificationMessage
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 @Transactional(rollbackFor = [Exception::class])
 class NotificationServiceImpl(
+    private val notificationPort: NotificationPort,
     private val queryNotificationPort: QueryNotificationPort,
     private val commandNotificationPort: CommandNotificationPort
 ) : NotificationService {
@@ -19,6 +25,31 @@ class NotificationServiceImpl(
             }
 
             commandNotificationPort.saveAlarm(deviceToken, alarmTime)
+        }
+    }
+
+    override fun sendAlarm() {
+        val currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
+
+        val notificationList = queryNotificationPort.findByAlarmTime(currentTime)
+
+        if (notificationList.isNotEmpty()) {
+            val notificationAlarm = NotificationAlarm(
+                title = NotificationMessage.ALARM_MESSAGE.title,
+                body = NotificationMessage.ALARM_MESSAGE.body,
+                writer = "ON°C"
+            )
+
+            when (notificationList.size) {
+                1 -> notificationPort.sendSingleNotification(
+                    deviceToken = notificationList.first().deviceToken,
+                    notificationAlarm = notificationAlarm
+                ) else ->
+                    notificationPort.sendMultipleNotification(
+                    deviceTokens = notificationList.map { it.deviceToken },
+                    notificationAlarm = notificationAlarm
+                )
+            }
         }
     }
 }
