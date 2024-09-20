4ecf9b385d0517cd3ab7e390a7b31a293ac1d7b4
uuuuuuuk <s22034@gsm.hs.kr>
2024-09-20 15:08:20 +0900

    create :: ScheduleConfig

---
+++ src/main/kotlin/com/ondosee/global/config/ScheduleConfig.kt
@@ -0,0 +1,8 @@
+package com.ondosee.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class ScheduleConfig
